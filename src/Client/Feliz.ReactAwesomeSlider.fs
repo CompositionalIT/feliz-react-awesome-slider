fa1ee46b938e9afdc12ae09960307cf18fd97ced
Akash Mair <akash@compositional-it.com>
2021-05-05 13:51:26 +0100

    trying to render children without sepearting them

---
+++ src/Client/Feliz.ReactAwesomeSlider.fs
@@ -0,0 +1,43 @@
+﻿module ReactAwesomeSlider
+
+open Fable.Core
+open Fable.Core.JsInterop
+open Fable.React
+open Feliz
+
+let reactAwesomeSlider: obj = importDefault "react-awesome-slider"
+
+importAll "react-awesome-slider/dist/custom-animations/cube-animation.css"
+importAll "react-awesome-slider/dist/custom-animations/fall-animation.css"
+importAll "react-awesome-slider/dist/custom-animations/fold-out-animation.css"
+importAll "react-awesome-slider/dist/custom-animations/open-animation.css"
+importAll "react-awesome-slider/dist/custom-animations/scale-out-animation.css"
+importAll "react-awesome-slider/dist/styles.css"
+//let splitChildProps props =
+//    let (children, props) =
+//        props
+//        |> unbox<(string*obj) seq>
+//        |> Seq.toArray
+//        |> Array.partition (fun (key,_) -> key = "children")
+//
+//    let children = children |> Array.tryLast |> Option.map snd |> Option.toObj
+//
+//    {| Props = props; Children = children |}
+
+[<Erase>]
+type IAwesomeSliderProperty = interface end
+//    abstract animation: string
+//    abstract styles: obj
+//    abstract children: obj
+
+module Interop =
+    let inline mkSliderAttr (key: string) (value: obj) : IAwesomeSliderProperty = unbox (key, value)
+
+
+type AwesomeSlider =
+    static member inline animation (s: string) = unbox<IAwesomeSliderProperty>("animation" ==> s)
+    static member inline selected (number: int) = Interop.mkSliderAttr "selected" number
+    static member inline children (e: ReactElement list) = Interop.mkSliderAttr "children" (prop.children e)
+    static member inline create (props: IAwesomeSliderProperty list) =
+        //let elements = splitChildProps props
+        Interop.reactApi.createElement(reactAwesomeSlider, createObj !!props)
---
+++ src/Client/Index.fs
@@ -0,0 +1,104 @@
+module Index
+
+open Elmish
+open Fable.Remoting.Client
+open Shared
+
+type Model = { Todos: Todo list; Input: string }
+
+type Msg =
+    | GotTodos of Todo list
+    | SetInput of string
+    | AddTodo
+    | AddedTodo of Todo
+
+let todosApi =
+    Remoting.createApi ()
+    |> Remoting.withRouteBuilder Route.builder
+    |> Remoting.buildProxy<ITodosApi>
+
+let init () : Model * Cmd<Msg> =
+    let model = { Todos = []; Input = "" }
+
+    let cmd =
+        Cmd.OfAsync.perform todosApi.getTodos () GotTodos
+
+    model, cmd
+
+let update (msg: Msg) (model: Model) : Model * Cmd<Msg> =
+    match msg with
+    | GotTodos todos -> { model with Todos = todos }, Cmd.none
+    | SetInput value -> { model with Input = value }, Cmd.none
+    | AddTodo ->
+        let todo = Todo.create model.Input
+
+        let cmd =
+            Cmd.OfAsync.perform todosApi.addTodo todo AddedTodo
+
+        { model with Input = "" }, cmd
+    | AddedTodo todo ->
+        { model with
+              Todos = model.Todos @ [ todo ] },
+        Cmd.none
+
+open Feliz
+open Feliz.Bulma
+open ReactAwesomeSlider
+let navBrand =
+    Bulma.navbarBrand.div [
+        Bulma.navbarItem.a [
+            prop.href "https://safe-stack.github.io/"
+            navbarItem.isActive
+            prop.children [
+                Html.img [
+                    prop.src "/favicon.png"
+                    prop.alt "Logo"
+                ]
+            ]
+        ]
+    ]
+
+let containerBox (model: Model) (dispatch: Msg -> unit) =
+    Bulma.box [
+        Bulma.content [
+            Html.ol [
+                for todo in model.Todos do
+                    Html.li [ prop.text todo.Description ]
+            ]
+        ]
+        Bulma.field.div [
+            field.isGrouped
+            prop.children [
+                Bulma.control.p [
+                    control.isExpanded
+                    prop.children [
+                        Bulma.input.text [
+                            prop.value model.Input
+                            prop.placeholder "What needs to be done?"
+                            prop.onChange (fun x -> SetInput x |> dispatch)
+                        ]
+                    ]
+                ]
+                Bulma.control.p [
+                    Bulma.button.a [
+                        color.isPrimary
+                        prop.disabled (Todo.isValid model.Input |> not)
+                        prop.onClick (fun _ -> dispatch AddTodo)
+                        prop.text "Add"
+                    ]
+                ]
+            ]
+        ]
+    ]
+
+let view (model: Model) (dispatch: Msg -> unit) =
+    AwesomeSlider.create [
+        AwesomeSlider.selected 3
+        AwesomeSlider.children [
+           Html.div "Hello"
+           Html.div "From"
+           Html.div "React"
+           Html.div "Awesome"
+           Html.div "Slider"
+        ]
+    ]
