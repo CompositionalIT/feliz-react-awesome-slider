7908618f584287762741ea8c03a7183673009d7b
Akash Mair <akash@compositional-it.com>
2021-05-05 19:38:39 +0100

    working component

--- src/Client/Feliz.ReactAwesomeSlider.fs
+++ src/Client/Feliz.ReactAwesomeSlider.fs
@@ -2,7 +2,6 @@
 
 open Fable.Core
 open Fable.Core.JsInterop
-open Fable.React
 open Feliz
 
 let reactAwesomeSlider: obj = importDefault "react-awesome-slider"
@@ -13,30 +12,42 @@ importAll "react-awesome-slider/dist/custom-animations/fold-out-animation.css"
 importAll "react-awesome-slider/dist/custom-animations/open-animation.css"
 importAll "react-awesome-slider/dist/custom-animations/scale-out-animation.css"
 importAll "react-awesome-slider/dist/styles.css"
-//let splitChildProps props =
-//    let (children, props) =
-//        props
-//        |> unbox<(string*obj) seq>
-//        |> Seq.toArray
-//        |> Array.partition (fun (key,_) -> key = "children")
-//
-//    let children = children |> Array.tryLast |> Option.map snd |> Option.toObj
-//
-//    {| Props = props; Children = children |}
+
 
 [<Erase>]
 type IAwesomeSliderProperty = interface end
-//    abstract animation: string
-//    abstract styles: obj
-//    abstract children: obj
 
-module Interop =
-    let inline mkSliderAttr (key: string) (value: obj) : IAwesomeSliderProperty = unbox (key, value)
 
+type Animation =
+    | FoldOutAnimation
+    | CubeAnimation
+    | FallAnimation
+    | OpenAnimation
+    | ScaleOutAnimation
+    with static member toJSValue = function
+            | FoldOutAnimation -> "foldOutAnimation"
+            | CubeAnimation -> "cubeAnimation"
+            | FallAnimation -> "fallAnimation"
+            | OpenAnimation -> "openAnimation"
+            | ScaleOutAnimation -> "scaleOutAnimation"
+
+let (=>) key value = unbox<IAwesomeSliderProperty>(key ==> value)
 
 type AwesomeSlider =
-    static member inline animation (s: string) = unbox<IAwesomeSliderProperty>("animation" ==> s)
-    static member inline selected (number: int) = Interop.mkSliderAttr "selected" number
+    static member inline animation (a: Animation) = "animation" => (Animation.toJSValue a)
+
+    static member inline name (s: string) = "name" => s
+
+    static member inline bullets (b: bool) = "bullets" => b
+    static member inline organicArrows (b: bool) = "organicArrows" => b
+    static member inline fillParent (b: bool) = "fillParent" => b
+    static member inline infinite (b: bool) = "infinite" => b
+    static member inline mobileTouch (b: bool) = "mobileTouch" => b
+    static member inline buttons (b: bool) = "buttons" => b
+
+
+    static member inline selected (number: int) = "selected" => number
+    static member inline transitionDelay (number: int) = "transitionDelay" => number
     static member inline children (e: ReactElement list) = unbox<IAwesomeSliderProperty>(prop.children e)
-    static member inline create (props: IAwesomeSliderProperty list) =
+    static member inline create (props) =
         Interop.reactApi.createElement(reactAwesomeSlider, createObj !!props)
--- src/Client/Index.fs
+++ src/Client/Index.fs
@@ -93,7 +93,8 @@ let containerBox (model: Model) (dispatch: Msg -> unit) =
 
 let view (model: Model) (dispatch: Msg -> unit) =
     AwesomeSlider.create [
-        //AwesomeSlider.selected 3
+        AwesomeSlider.animation FallAnimation
+        AwesomeSlider.selected 3
         AwesomeSlider.children [
            Html.div "Hello"
            Html.div "From"
